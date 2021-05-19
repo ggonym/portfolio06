eb3a03aa15e15ec97bc4eb4786aec0b2e706febd
gym315 <71267287+gym315@users.noreply.github.com>
2021-05-19 23:53:54 +0900

    Add files via upload

--- music/js/swiper.js
+++ music/js/swiper.js
@@ -1,7 +1,11 @@
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        direction: "vertical",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    </script>
